3e34c35bf64e5323ab709a81e8d74f342cb5e070
ParkKiHun <miy1205@naver.com>
2023-04-03 18:07:25 +0900

    Initial commit

---
+++ Assets/Undead Survivor/Script/Player.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
+
+    // Awake 시작할 때 한번만 실행되는 생명주기 (초기화)
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");*/
+    }
+
+    void FixedUpdate()
+    {
+        // 1. 힘을 준다.
+        //rigid.AddForce(inputVec);
+
+        // 2. 속도 제어
+        //rigid.velocity = inputVec;
+
+        // 3. 위치 이동
+        //normalized : 벡터 값의 크기가 1이 되도록 좌표가 수정된 값
+
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
+                
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }
+    
+    // 프레임이 종료 되기 전 실행되는 생명주기 함수
+    void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        if (inputVec.x != 0)
+            spriter.flipX = inputVec.x < 0;
+
+    }
+}
